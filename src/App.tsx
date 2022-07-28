809d3673383d8af338ed869beadedfdc1fa87780
kevinchang <kevinjchang98@gmail.com>
2022-07-27 17:17:39 -0700

    initial firestore implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import {
+    collection,
+    DocumentData,
+    getDocs,
+    query,
+    QueryDocumentSnapshot,
+} from 'firebase/firestore';
+import { firestore } from './firestore/firestore';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const [canvasData, setCanvasData] = useState<
+        QueryDocumentSnapshot<DocumentData>[]
+    >([]);
+
+    // Canvas collection from firestore
+    const canvasCollection = collection(firestore, 'canvas');
+
+    // Getter for canvas data
+    const getCanvasData = async () => {
+        const canvasQuery = query(canvasCollection);
+        const querySnapshot = await getDocs(canvasQuery);
+
+        const result: QueryDocumentSnapshot<DocumentData>[] = [];
+
+        querySnapshot.forEach((snapshot) => {
+            result.push(snapshot);
+        });
+
+        setCanvasData(result);
+    };
+
+    return (
+        <div className="App">
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src="/vite.svg" className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://reactjs.org" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            Sample canvas data fetched from firestore:
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </div>
+    );
 }
 
-export default App
+export default App;

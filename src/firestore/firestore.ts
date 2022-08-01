7f79cbd065fe7b5446096a81f5c5394a074d816a
kevinchang <kevinjchang98@gmail.com>
2022-07-31 17:57:45 -0700

    update to use local environment variables

--- src/firestore/firestore.ts
+++ src/firestore/firestore.ts
@@ -1,30 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-import data from '../../firebase_config.json';
-
-if (data.isServer) {
-    initializeApp({
-        apiKey: import.meta.env.VITE_VERCEL_FIREBASE_API_KEY,
-        authDomain: import.meta.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN,
-        projectId: import.meta.env.VITE_VERCEL_FIREBASE_PROJECT_ID,
-        storageBucket: import.meta.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env
-            .VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID,
-        appId: import.meta.env.VITE_VERCEL_FIREBASE_APP_ID,
-        measurementId: import.meta.env.VITE_VERCEL_FIREBASE_MEASUREMENT_ID,
-    });
-} else {
-    initializeApp({
-        apiKey: data.apiKey,
-        authDomain: data.authDomain,
-        projectId: data.projectId,
-        storageBucket: data.storageBucket,
-        messagingSenderId: data.messagingSenderId,
-        appId: data.appId,
-        measurementId: data.measurementId,
-    });
-}
+initializeApp({
+    apiKey: import.meta.env.VITE_VERCEL_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_VERCEL_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_VERCEL_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_VERCEL_FIREBASE_MEASUREMENT_ID,
+});
 
 const firestore = getFirestore();
 

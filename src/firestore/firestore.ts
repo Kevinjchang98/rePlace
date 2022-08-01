792c3880eb5790fc7dae625d45cbc4e9e29d122b
kevinchang <kevinjchang98@gmail.com>
2022-07-31 17:30:56 -0700

    convert process.env to import.meta.env

--- src/firestore/firestore.ts
+++ src/firestore/firestore.ts
@@ -4,17 +4,18 @@ import { getFirestore } from 'firebase/firestore';
 import data from '../../firebase_config.json';
 
 if (data.isServer) {
-    console.log(process.env.VITE_VERCEL_URL);
+    console.log(import.meta.env.VITE_VERCEL_URL);
     console.log('Server');
-    console.log(process.env.VITE_VERCEL_FIREBASE_API_KEY);
+    console.log(import.meta.env.VITE_VERCEL_FIREBASE_API_KEY);
     initializeApp({
-        apiKey: process.env.VITE_VERCEL_FIREBASE_API_KEY,
-        authDomain: process.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.VITE_VERCEL_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.VITE_VERCEL_FIREBASE_APP_ID,
-        measurementId: process.env.VITE_VERCEL_FIREBASE_MEASUREMENT_ID,
+        apiKey: import.meta.env.VITE_VERCEL_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_VERCEL_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env
+            .VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_VERCEL_FIREBASE_APP_ID,
+        measurementId: import.meta.env.VITE_VERCEL_FIREBASE_MEASUREMENT_ID,
     });
 } else {
     initializeApp({

8f7bac9eb4982bdff696534b3efd47c0edf00b45
kevinchang <kevinjchang98@gmail.com>
2022-07-31 17:31:58 -0700

    remove VITE_VERCEL prefix

--- src/firestore/firestore.ts
+++ src/firestore/firestore.ts
@@ -4,18 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import data from '../../firebase_config.json';
 
 if (data.isServer) {
-    console.log(import.meta.env.VITE_VERCEL_URL);
-    console.log('Server');
-    console.log(import.meta.env.VITE_VERCEL_FIREBASE_API_KEY);
     initializeApp({
-        apiKey: import.meta.env.VITE_VERCEL_FIREBASE_API_KEY,
-        authDomain: import.meta.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN,
-        projectId: import.meta.env.VITE_VERCEL_FIREBASE_PROJECT_ID,
-        storageBucket: import.meta.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env
-            .VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID,
-        appId: import.meta.env.VITE_VERCEL_FIREBASE_APP_ID,
-        measurementId: import.meta.env.VITE_VERCEL_FIREBASE_MEASUREMENT_ID,
+        apiKey: import.meta.env.FIREBASE_API_KEY,
+        authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.FIREBASE_APP_ID,
+        measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
     });
 } else {
     initializeApp({
